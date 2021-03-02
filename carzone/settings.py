49774664d206fc148a0d53ba238c8039b2dc29ce
Edicemi <victoriataiwo1998@gmail.com>
2021-03-02 13:58:22 +0100

    completed teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': '64648612',
+        'HOST': 'localhost',
     }
 }
 
@@ -123,3 +126,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'carzone/static',
 ]
+
+# settings for media
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
