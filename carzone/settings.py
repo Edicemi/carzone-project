c615ad3e943d07b189b8eccc82f559670365f114
Edicemi <victoriataiwo1998@gmail.com>
2021-03-21 15:50:32 +0100

    changes in settings

--- carzone/settings.py
+++ carzone/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 
 from pathlib import Path
-import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = 'c93)lu3qg!=7tpz7k%8r2r*&6_k$x)vt7d1shkp5-po5*sb-p$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['edicemi1.pythonanywhere.com']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
@@ -170,4 +171,4 @@ EMAIL_USE_TLS = True
 # whitenoise settings
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+
